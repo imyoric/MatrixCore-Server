43876c90fb446a7ac01ffe89fcd00dfedb5e8286
md_5 <git@md-5.net>
2014-07-04 13:28:45 +1000

    Further Seed Customisation
    Allow server admins that really want to to customise the seeds used in world generation even further.

--- src/main/java/net/minecraft/server/level/PlayerChunkMap.java
+++ src/main/java/net/minecraft/server/level/PlayerChunkMap.java
@@ -212,7 +212,7 @@ public class PlayerChunkMap extends IChunkLoader implements PlayerChunk.d {
             this.randomState = RandomState.create(GeneratorSettingBase.dummy(), (HolderGetter) iregistrycustom.lookupOrThrow(Registries.NOISE), j);
         }
 
-        this.chunkGeneratorState = chunkgenerator.createState(iregistrycustom.lookupOrThrow(Registries.STRUCTURE_SET), this.randomState, j);
+        this.chunkGeneratorState = chunkgenerator.createState(iregistrycustom.lookupOrThrow(Registries.STRUCTURE_SET), this.randomState, j, worldserver.spigotConfig); // Spigot
         this.mainThreadExecutor = iasynctaskhandler;
         ThreadedMailbox<Runnable> threadedmailbox = ThreadedMailbox.create(executor, "worldgen");
 
